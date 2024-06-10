cded429e160918ba38a3935aa433a5a51dd147a1
Intannn20 <132120208+Intannn20@users.noreply.github.com>
2024-06-10 19:11:10 +0700

    Delete foot-measurement directory

--- foot-measurement/pages/home.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-
-def show():
-    st.title("Home")
-    st.write("Welcome to the Foot Measurement Application!")
-    st.image("foot_measurement.jpg", use_column_width=True)
